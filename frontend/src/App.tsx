280570923872ae31b187619010875b4f4c3605da
Harish Rao <harishraodugyala@gmail.com>
2024-06-14 13:38:58 +0530

    Fix: Minor error fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from '@/pages/auth/Login';
 import { Home } from '@/pages/Home';
 import { AddMember } from '@/pages/addMember';
-import { AddModel }  from '@/pages/addModel';
 import {ChangePassword} from '@/pages/auth/Profile';
-import { AddAdmin } from '@/pages/addAdmin';
 import AddProduct from '@/pages/addProduct';
 import AddComponent from '@/pages/addComponent';
 import AddPart from '@/pages/addPart';
@@ -19,7 +17,6 @@ const App = () => {
           <Route path='/change-password' element={<ChangePassword />} />
           <Route path='/add-product' element={<AddProduct />} />
           <Route path='/add-member' element={<AddMember />} />
-          <Route path='/add-admin' element={<AddAdmin />} />
           <Route path='/add-product/:productName' element={<AddComponent />} /> 
           <Route path='/add-product/:productName/:componentLabel' element={<AddPart />} />
 
