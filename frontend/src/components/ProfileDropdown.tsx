6c8e78b254243a03e1ad50adb8d931e818ee054d
SUMASWETHAPALLAPOLU <sumaswetha@gmail.com>
2024-06-18 15:38:43 +0530

    feature:added fuctionality to profiledropdown

--- frontend/src/components/ProfileDropdown.tsx
+++ frontend/src/components/ProfileDropdown.tsx
@@ -1,37 +1,46 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger,
- } from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu"
 import Profile from '@/assets/icons/profile.svg'
 import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 
 const ProfileDropdown = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 815);
-  
-    useEffect(() => {
-      const handleResize = () => {
-        setIsMobile(window.innerWidth <= 815);
-      };
-  
-      window.addEventListener('resize', handleResize);
-  
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  return (
-    <div className={`cursor-pointer ${!isMobile ? 'absolute top-4 right-4' : 'relative' }`}>
-      <DropdownMenu>
-    <DropdownMenuTrigger>
-     <img src={Profile} width={40}/>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent className="cursor-pointer">
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>Profile</DropdownMenuItem>
-      <DropdownMenuItem>Change Password</DropdownMenuItem>
-      <DropdownMenuItem className="text-red-600 hover:text-white font-bold ">Logout</DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
-    </div>
-  )
+ const [isMobile, setIsMobile] = useState(window.innerWidth <= 815);
+ const navigate = useNavigate();
+
+ const handleLogout = () => {
+   navigate('/');
+ }
+ const handlePassword = () => {
+   navigate('/change-password');
+ }
+
+   useEffect(() => {
+     const handleResize = () => {
+       setIsMobile(window.innerWidth <= 815);
+     };
+ 
+     window.addEventListener('resize', handleResize);
+ 
+     return () => window.removeEventListener('resize', handleResize);
+   }, []);
+ return (
+   <div className={`cursor-pointer ${!isMobile ? 'absolute top-4 right-4' : 'relative' }`}>
+     <DropdownMenu>
+   <DropdownMenuTrigger>
+    <img src={Profile} width={40}/>
+   </DropdownMenuTrigger>
+   <DropdownMenuContent className="cursor-pointer">
+     <DropdownMenuLabel>My Account</DropdownMenuLabel>
+     <DropdownMenuSeparator />
+     <DropdownMenuItem>Profile</DropdownMenuItem>
+     <DropdownMenuItem onClick={()=>{handlePassword()}}>Change Password</DropdownMenuItem>
+     <DropdownMenuItem onClick={()=>{handleLogout()}} className="text-red-600 hover:text-white font-bold ">Logout</DropdownMenuItem>
+   </DropdownMenuContent>
+ </DropdownMenu>
+   </div>
+ )
 }
 
 export default ProfileDropdown;
