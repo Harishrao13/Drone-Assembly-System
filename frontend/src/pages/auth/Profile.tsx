0ec104a9c98bb33929f5ba15cbb8528c492f36e4
harishrao13 <harishraodugyala@gmail.com>
2024-07-02 11:32:06 +0530

    Made minor changes to UI

--- frontend/src/pages/auth/Profile.tsx
+++ frontend/src/pages/auth/Profile.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Layout from '@/pages/layout';
 
-
 import {
   Card,
   CardContent,
@@ -11,6 +10,7 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
+import { Label } from "@radix-ui/react-label";
 
 export function ChangePassword() {
   const [newPassword, setNewPassword] = useState("");
@@ -18,50 +18,55 @@ export function ChangePassword() {
 
   return (
     <Layout>
-    <div className="change-password flex items-center justify-center h-screen w-full">
-      <Card className="p-6 max-w-md w-full">
-        <CardHeader>
-          <CardTitle>Change Password</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <form>
-            <div className="grid w-max gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Input id="name" placeholder="UserID" />
+      <div className="change-password flex items-center justify-center h-screen w-full">
+        <Card className="p-6 max-w-md w-full flex flex-col">
+          <CardHeader>
+            <CardTitle>Change Password</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3 flex flex-col gap-4 font-semibold">
+            <form className="w-full">
+              <div className="grid gap-4 form-container">
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="cpassword">Current Password</Label>
+                  <Input
+                    id="cpassword"
+                    type="password"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="npassword">New Password</Label>
+                  <Input
+                    id="npassword"
+                    type="password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="cnpassword">Confirm New Password</Label>
+                  <Input
+                    id="cnpassword"
+                    type="password"
+                    value={confirmNewPassword}
+                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                    className="w-full"
+                  />
+                </div>
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Input
-                  id="cpassword"
-                  placeholder="Current Password"
-                  type="password"
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Input
-                  type="password"
-                  placeholder="Enter new password"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Input
-                  type="password"
-                  placeholder="Confirm new password"
-                  value={confirmNewPassword}
-                  onChange={(e) => setConfirmNewPassword(e.target.value)}
-                />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        {newPassword === confirmNewPassword && (
-          <CardFooter className="flex justify-between">
-            <Button>Change Password</Button>
+            </form>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            {newPassword !== confirmNewPassword && (
+              <h3 className="text-red-600">The passwords do not match!</h3>
+            )}
+            <Button disabled={newPassword !== confirmNewPassword} className='btn-primary'>
+              Update Password
+            </Button>
           </CardFooter>
-        )}
-      </Card>
-    </div>
+        </Card>
+      </div>
     </Layout>
   );
 }
